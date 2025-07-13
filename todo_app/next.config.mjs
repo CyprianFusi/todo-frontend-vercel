ab27421fdcf5f329decd57e29518528e1d7aca2f
Cyprian Fusi <44713120+CyprianFusi@users.noreply.github.com>
2025-07-13 23:42:41 +0100

    updated next.config.mjs

--- todo_app/next.config.mjs
+++ todo_app/next.config.mjs
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// Import the 'path' module to resolve paths
 
-export default nextConfig;
+const path = require('path');
+const nextConfig = {
+
+  reactStrictMode: true,
+
+  // Extend the Webpack configuration
+
+  webpack: (config) => {
+
+    // Set up an alias '@' that points to the root directory of the project
+
+    config.resolve.alias['@'] = path.resolve(__dirname);
+
+    return config;
+
+  },
+
+}
+
+module.exports = nextConfig;
