e56985fa87d0fdeee425c80f3032961b2321fa04
Cyprian Fusi <44713120+CyprianFusi@users.noreply.github.com>
2025-07-13 17:04:43 +0100

    First commit

---
+++ todo_app/components/layout.js
@@ -0,0 +1,12 @@
+'use client'
+
+import styles from '../styles/layout.module.css'
+
+export default function Layout(props) {
+  return (
+    <div className={styles.layout}>
+      <h1 className={styles.title}>To Do</h1>
+      {props.children}
+    </div>
+  )
+}
